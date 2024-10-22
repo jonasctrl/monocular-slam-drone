dfb9a1f2c8e0122a76ecd28596b43d6e2900bb75
linper <llinas7879@gmail.com>
2024-10-22 20:23:34 +0300

    fixed pathing

--- mapper-nav-node/nav/scripts/a_star.py
+++ mapper-nav-node/nav/scripts/a_star.py
@@ -21,7 +21,8 @@ def get_neighbors(node, grid):
                 # Ensure the neighbor is within the bounds of the grid
                 if 0 <= new_x < grid.shape[0] and 0 <= new_y < grid.shape[1] and 0 <= new_z < grid.shape[2]:
                     # Add only valid neighbors (e.g., avoiding obstacles)
-                    if cfg.occup_min <= grid[new_x, new_y, new_z] < cfg.occup_thr: 
+                    # if cfg.occup_min <= grid[new_x, new_y, new_z] < cfg.occup_thr:
+                    if grid[new_x, new_y, new_z] == 0:
                         neighbors.append((new_x, new_y, new_z))
                     # else:
                         # print(f"not passible={(new_x, new_y, new_z)}")
